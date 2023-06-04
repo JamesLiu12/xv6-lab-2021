999658b275c5463d75946b8cf8401d60ec107373
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2023-06-04 22:57:55 +0800

    Finish Lab 1

--- user/primes.c
+++ user/primes.c
@@ -1,49 +1,43 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-void prime(int *p_left){
-	int num;
-	int p_right[2];
-	if(read(p_left[0], &num, 4) == 0){
-		exit(0);
-	}else{
-		printf("prime %d\n", num);
+void primes(int *pl){
+	int num, pr[2];
+	pipe(pr);
+	if (read(pl[0], &num, sizeof(int))) fprintf(1, "prime %d\n", num);
+	else exit(0);
+	if (fork() == 0){
+		close(pr[1]);
+		primes(pr);
+		close(pr[0]);
 	}
-	pipe(p_right);
-	if(fork() == 0){
-		close(p_right[1]);
-		prime(p_right);
-		close(p_right[0]);
-	}else{
-		close(p_right[0]);
-		int next;
-		while(read(p_left[0], &next, 4)){
-			if(next % num != 0){
-				write(p_right[1], &next, 4);
-			}
+	else {
+		int nxt;
+		close(pl[1]);
+		close(pr[0]);
+		while (read(pl[0], &nxt, sizeof(int))){
+			if (nxt % num) write(pr[1], &nxt, sizeof(int));
 		}
-		close(p_right[1]);
+		close(pl[0]);
+		close(pr[1]);
 		wait(0);
 		exit(0);
 	}
 }
 
-int main(int argc, char *argv[]){
+int main() {
 	int p[2];
 	pipe(p);
-	if(fork() > 0){	
+	if (fork() == 0) {
+		close(p[1]);
+		primes(p);
 		close(p[0]);
-		for(int i = 2; i <= 35; i++){
-			write(p[1], &i, 4);		
-		}
+	}
+	else{
+		close(p[0]);
+		for (int i = 2; i <= 35; i++) write(p[1], &i, sizeof(int));
 		close(p[1]);
 		wait(0);
-		exit(0);
-	}else{
-		close(p[1]);
-		prime(p);
-		close(p[0]);
-		exit(0);
-	}			
+	}
+	exit(0);
 }
-
